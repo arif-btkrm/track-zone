9058875456fe6b4ebbf5f28981f380f55268b536
arif-btkrm <arif.btkrm@gmail.com>
2024-06-25 03:28:59 +0600

    updated packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
